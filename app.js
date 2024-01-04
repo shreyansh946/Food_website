551e4663bb3421db64280ef4c7a094b7fdc0ef43
shreyansh946 <shreyanshjain648@gmail.com>
2024-01-04 21:37:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const parent = React.createElement("div",
+    { id: "parent" },
+    [
+            React.createElement(
+                "div",
+                { id: "child" }, [
+                React.createElement("h1", {}, "i am h1 tag"),
+                React.createElement("h2", {}, "i am h2 tag")
+                ]),
+                React.createElement(
+                    "div",
+                    { id: "child2" }, [
+                    React.createElement("h1", {}, "i am h1 tag"),
+                    React.createElement("h2", {}, "i am h2 tag")
+                    ]),
+    ]
+);
+
+
+const heading = React.createElement("h1", { id: "heading" }, "hello world from react");
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
