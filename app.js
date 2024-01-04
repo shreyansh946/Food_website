3f9299ad2a6ed3179106749149eddcaeff1bc7ee
shreyansh946 <shreyanshjain648@gmail.com>
2024-01-04 21:48:55 +0530

    this is container method add

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
+const heading2 =  React.createElement(
+    "div",
+    { id: "child2" }, [
+    React.createElement("h1", {}, "i am h1 tag"),
+    React.createElement("h2", {}, "i am h2 tag")
+    ]);
+
+    const heading1 = React.createElement(
+        "div",
+        { id: "child" }, [
+        React.createElement("h1", {}, "i am h1 tag"),
+        React.createElement("h2", {}, "i am h2 tag")
+        ]);
 
 const parent = React.createElement("div",
     { id: "parent" },
     [
-            React.createElement(
-                "div",
-                { id: "child" }, [
-                React.createElement("h1", {}, "i am h1 tag"),
-                React.createElement("h2", {}, "i am h2 tag")
-                ]),
-                React.createElement(
-                    "div",
-                    { id: "child2" }, [
-                    React.createElement("h1", {}, "i am h1 tag"),
-                    React.createElement("h2", {}, "i am h2 tag")
-                    ]),
+            heading1,
+                heading2
     ]
 );
 
