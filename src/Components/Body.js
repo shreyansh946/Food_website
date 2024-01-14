2696be63934013af768c7a36b79f399b4417c972
shreyansh946 <shreyanshjain648@gmail.com>
2024-01-14 23:16:09 +0530

    data add from the mock data

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,18 +1,22 @@
-import RestaurantCard from "./Components/RestaurantCard";
+import React from "react";
+import RestaurantCard from "./RestaurantCard";
+import resDataList from "../Utils/mockData";
 
-
-const Body = () =>{
-    return (
-        <div className="body">
-            <div className="search">
-                    Search
-            </div>
-            <div className="res-container">
-                <RestaurantCard key={restaurant.data.id}
-                 resData={restaurant} />
-            </div>
+const Body = () => {
+  return (
+   <div className="body" >
+    <div className="search">
+        <div className="res-container">
+            {
+                resDataList.map((restaurant) =>{
+                    <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+                })
+            }
         </div>
-    )
-}
+      
+      </div>
+    </div>
+  );
+};
 
 export default Body;
