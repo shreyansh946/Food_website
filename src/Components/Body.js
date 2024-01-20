5f2ca347d6bd5e9a214c37ca639dd4c7dc0a97b6
Shreyansh946 <shreyanshjain648@gmail.com>
2024-01-20 20:10:28 +0530

    this is fetching live api data

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,24 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../Utils/mockData";
 import { useEffect, useState } from "react";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-  useEffect(() =>{
-        fetchData()
-  },[]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async () =>{
+  const fetchData = async () => {
+    try {
       const data = await fetch(
-"https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
 
-      const  json = await data.json();
+      const json = await data.json();
 
       console.log(json);
-      setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setListOfRestaurants(
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
+  };
 
   return (
     <div className="body">
@@ -36,8 +40,8 @@ const Body = () => {
           Top Filter Restaurant
         </button>
         <div className="res-container">
-          {listOfRestaurants.map((restaurant) => (
-            <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          {listOfRestaurants?.map((restaurant) => (
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           ))}
         </div>
       </div>
