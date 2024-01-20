d324bafe3d06ccfd54d50d17d3deb4ecd1536226
Shreyansh946 <shreyanshjain648@gmail.com>
2024-01-20 20:10:44 +0530

    this commnet add

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,17 @@
 const Header = () => {
   return (
     <header className='header'>
+      {/* Logo container */}
       <div className='logo-container'>
         <h1 className='logo'>
           LOGO
         </h1>
       </div>
+
+      {/* Navigation items */}
       <div className='nav-items'>
         <ul>
+          {/* List of navigation items */}
           <li>Home</li>
           <li>About</li>
           <li>Contact us</li>
