82090168f64ff1e32fdfb1d4cb00ded683bb89c3
Shreyansh946 <shreyanshjain648@gmail.com>
2024-01-20 20:11:01 +0530

    update restaurantcard app

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -3,6 +3,7 @@ import { CDN_URL } from "../Utils/constants";
 const RestaurantCard = (props) => {
     const { resData } = props;
 
+    // Destructuring properties from resData.info
     const {
         cloudinaryImageId,
         name,
@@ -10,21 +11,22 @@ const RestaurantCard = (props) => {
         cuisines,
         costForTwo,
         deliveryTime,
-    } = resData?.data;
+    } = resData?.info;
 
     return (
         <div className="res-card">
-            <img className="res-logo"
+            {/* Rendering the restaurant logo */}
+            <img
+                className="res-logo"
                 alt="res-logo"
                 src={CDN_URL + cloudinaryImageId}
-        />
+            />
             <h3>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating} stars</h4>
             <h4>₹{costForTwo / 100} FOR TWO</h4>
-            <h4> {deliveryTime} Mins</h4>
-      </div>
-
+            <h4>{deliveryTime} Mins</h4>
+        </div>
     );
 };
 
